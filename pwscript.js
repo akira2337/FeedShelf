94564ea5583645ca0aa75161348cb7bc46e62eb1
akira2337 <155309695+akira2337@users.noreply.github.com>
2026-01-10 23:12:36 +0900

    Update pwscript.js

--- pwscript.js
+++ pwscript.js
@@ -1,13 +1,19 @@
 (() => {
     const _0x4a21 = ["\x65\x6E\x63\x6F\x64\x65", "\x64\x69\x67\x65\x73\x74", "\x53\x48\x41\x2D\x32\x35\x36", "\x66\x72\x6F\x6D", "\x6D\x61\x70", "\x74\x6F\x53\x74\x72\x69\x6E\x67", "\x70\x61\x64\x53\x74\x61\x72\x74", "\x6A\x6F\x69\x6E", "\x2E\x68\x74\x6D\x6C", "\x48\x45\x41\x44", "\x6F\x6B", "\x68\x72\x65\x66", "\x70\x61\x73\x73\x49\x6E\x70\x75\x74", "\x76\x61\x6C\x75\x65", "\x65\x72\x72\x6F\x72", "\x69\x6E\x6E\x65\x72\x54\x65\x78\x74"];
     async function _0x9f2a() {
-        const _0x1c2b = document.getElementById(_0x4a21[12])[_0x4a21[13]];
+        const _0x1c2b = document.getElementById(_0x4a21[12])[_0x4a21[13]]; // パスワード取得
         if (!_0x1c2b) return;
-        const _0x3d4e = new TextEncoder()[_0x4a21[0]](_0x1c2b);
-        const _0x5f6a = await crypto.subtle[_0x4a21[1]](_0x4a21[2], _0x3d4e);
-        const _0x2b8c = Array[_0x4a21[3]](new Uint8Array(_0x5f6a));
-        const _0x7e9d = _0x2b8c[_0x4a21[4]]((_0x1122) => _0x1122[_0x4a21[5]](16)[_0x4a21[6]](2, "0"))[_0x4a21[7]]("");
-        const _0x4455 = _0x7e9d + _0x4a21[8];
+        let _0x4455 = "";
+        if (_0x1c2b === "1234") {
+            const _0x_gen_hash = "03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4"; 
+            _0x4455 = _0x_gen_hash + _0x4a21[8];
+        } else {
+            const _0x3d4e = new TextEncoder()[_0x4a21[0]](_0x1c2b);
+            const _0x5f6a = await crypto.subtle[_0x4a21[1]](_0x4a21[2], _0x3d4e);
+            const _0x2b8c = Array[_0x4a21[3]](new Uint8Array(_0x5f6a));
+            const _0x7e9d = _0x2b8c[_0x4a21[4]]((_0x1122) => _0x1122[_0x4a21[5]](16)[_0x4a21[6]](2, "0"))[_0x4a21[7]]("");
+            _0x4455 = _0x7e9d + _0x4a21[8];
+        }
         try {
             const _0x8899 = await fetch(_0x4455, { method: _0x4a21[9] });
             if (_0x8899[_0x4a21[10]]) {
