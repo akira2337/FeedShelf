0f83dfcec1033924dc3e422490b86c3abb64077e
akira2337 <155309695+akira2337@users.noreply.github.com>
2026-01-10 13:58:05 +0900

    Update pwscript.js

--- pwscript.js
+++ pwscript.js
@@ -1,24 +1,16 @@
 (() => {
-    // このスコープ（{}の中）にある変数は、外のデベロッパーツールからは見えません
-    async function _secureProcess() {
-        const val = document.getElementById('passInput').value;
-        if (!val) return;
-
-        // ハッシュ化の計算
-        const msgUint8 = new TextEncoder().encode(val);
-        const hashBuffer = await crypto.subtle.digest('SHA-256', msgUint8);
-        const hashArray = Array.from(new Uint8Array(hashBuffer));
-        const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
-
-        // ページ遷移
-        window.location.href = hashHex + ".html";
-
+    const _0x52a1 = ["\x65\x6E\x63\x6F\x64\x65", "\x64\x69\x67\x65\x73\x74", "\x53\x48\x41\x2D\x32\x35\x36", "\x66\x72\x6F\x6D", "\x6D\x61\x70", "\x74\x6F\x53\x74\x72\x69\x6E\x67", "\x70\x61\x64\x53\x74\x61\x72\x74", "\x6A\x6F\x69\x6E", "\x2E\x68\x74\x6D\x6C", "\x68\x72\x65\x66", "\x70\x61\x73\x73\x49\x6E\x70\x75\x74", "\x76\x61\x6C\x75\x65", "\x65\x72\x72\x6F\x72", "\x69\x6E\x6E\x65\x72\x54\x65\x78\x74"];
+    async function _0x3b21() {
+        const _0x1a2c = document.getElementById(_0x52a1[10])[_0x52a1[11]];
+        if (!_0x1a2c) return;
+        const _0x4f12 = new TextEncoder()[_0x52a1[0]](_0x1a2c);
+        const _0x2d3e = await crypto.subtle[_0x52a1[1]](_0x52a1[2], _0x4f12);
+        const _0x9921 = Array[_0x52a1[3]](new Uint8Array(_0x2d3e));
+        const _0xbc42 = _0x9921[_0x52a1[4]]((_0x8821) => _0x8821[_0x52a1[5]](16)[_0x52a1[6]](2, "0"))[_0x52a1[7]]("");
+        window.location.href = _0xbc42 + _0x52a1[8];
         setTimeout(() => {
-            document.getElementById('error').innerText = "アクセス権限がありません";
+            document.getElementById(_0x52a1[12])[_0x52a1[13]] = "\u30a2\u30af\u30bb\u30b9\u6a29\u9650\u304c\u3042\u308a\u307e\u305b\u3093";
         }, 1000);
     }
-
-    // グローバルな window オブジェクトに、実行用の関数だけを紐付けます
-    // 名前を entry ではなく launch 等に変えるとさらに推測されにくくなります
-    window.launch = _secureProcess;
+    window.launch = _0x3b21;
 })();
